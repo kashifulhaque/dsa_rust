7ba58e57284d79a7c279a841811007006237c7c1
kashifulhaque <haque.kashiful7@gmail.com>
2020-07-23 15:37:38 +0530

    Implemented Binary Search (iteration)

---
+++ algo/search/binary/binary_search.rs
@@ -0,0 +1,22 @@
+// Binary Search using Iteration
+fn search(arr: &Vec<i32>, item: i32) -> i32 {
+    if arr.len() == 0 {
+        return -1;
+    }
+
+    let mut l: usize = 0;
+    let mut h: usize = arr.len() - 1;
+    while l <= h {
+        let mid: usize = (l + h) / 2;
+
+        if arr[mid] == item {
+            return mid as i32;
+        } else if item < arr[mid] {
+            h = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return -1;
+}
